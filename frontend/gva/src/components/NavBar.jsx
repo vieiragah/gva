e63e8e53088a275e93d86df640afd1ccd39e3002
vieiragah <107413566+vieiragah@users.noreply.github.com>
2023-04-13 00:51:31 -0300

    teste renderização condicional

--- frontend/gva/src/components/NavBar.jsx
+++ frontend/gva/src/components/NavBar.jsx
@@ -9,15 +9,15 @@ const NavBar = () => {
                 <h1><Link to='/'>GVA <span>Med</span></Link></h1>
                 <ul>
                     <li><Link to='/'>Setor</Link></li>
-                    <li><Link to='/register'>Cadastrar-se</Link></li>
                     <li><Link to='/login'>Entrar</Link></li>
- 
                 </ul>
             </Navgate>
         </header>
     </div>
   )
 }
+//                    <li><Link to='/register'>Cadastrar-se</Link></li>
+
 //                                       <li>{user ? "sair" : ""}</li>
 //uma renderização condicional no ultimo li, trazendo o status de usuário logado ele aparece o sair, caso contrado nada ou então algum css por trás para não criar aquele padding que ficaria no canto direto. (perguntar ao rafa)
 export default NavBar
